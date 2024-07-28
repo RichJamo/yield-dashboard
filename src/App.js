8b44b6a591d67571453398a1f526989bb70ffceb
Richard Jamieson <jamieson.rich@gmail.com>
2024-07-28 18:18:36 +0200

    Add tailwind styling, add rewards APY, etc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchYields } from './fetchYields'; // Import your fetchYields function
+import './YieldDashboard.css'; // Import CSS for styling
 
 const YieldDashboard = () => {
   const [yieldData, setYieldData] = useState([]);
@@ -19,34 +20,52 @@ const YieldDashboard = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Yield Dashboard</h1>
-      {error ? (
-        <p>{error}</p> // Display error message if any
-      ) : yieldData.length > 0 ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Protocol</th>
-              <th>Chain</th>
-              <th>Asset</th>
-              <th>Base APY</th>
-            </tr>
-          </thead>
-          <tbody>
-            {yieldData.map((item, index) => (
-              <tr key={index}>
-                <td>{item.protocol}</td>
-                <td>{item.chain}</td>
-                <td>{item.asset}</td>
-                <td>{item.apy}%</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>Loading...</p> // Show loading message while fetching data
-      )}
+    <div className="min-h-screen bg-gray-100 py-8 px-4">
+      <div className="max-w-7xl mx-auto bg-white shadow-lg rounded-lg p-8">
+        <h1 className="text-2xl font-bold mb-4">Yield Dashboard</h1>
+        {error ? (
+          <p className="text-red-500">{error}</p>
+        ) : yieldData.length > 0 ? (
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2 border-b-2 border-gray-300  text-left">Protocol</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-300  text-left">Chain</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-300  text-left">Asset</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-300  text-left">Total Supply</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-300  text-left">Base APY</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-300  text-left">Rewards APY</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-300  text-left">Total APY</th>
+                  <th className="px-4 py-2 border-b-2 border-gray-300 text-left relative group">
+                    Remaining Rewards Time
+                    <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 hidden w-48 bg-gray-700 text-white text-xs rounded-lg py-2 px-4 group-hover:block">
+                      The time left until the reward distribution period ends.
+                      <span className="absolute top-full left-1/2 transform -translate-x-1/2 border-8 border-gray-700 border-transparent"></span>
+                    </span>
+                  </th>            
+                </tr>
+              </thead>
+              <tbody>
+                {yieldData.map((item, index) => (
+                  <tr key={index} className="hover:bg-gray-100">
+                    <td className="px-4 py-2 border-b">{item.protocol}</td>
+                    <td className="px-4 py-2 border-b">{item.chain}</td>
+                    <td className="px-4 py-2 border-b">{item.asset}</td>
+                    <td className="px-4 py-2 border-b">{item.totalSupply}</td>
+                    <td className="px-4 py-2 border-b">{item.apy}%</td>
+                    <td className="px-4 py-2 border-b">{item.rewards}%</td>
+                    <td className="px-4 py-2 border-b">{item.totalAPY}%</td>
+                    <td className="px-4 py-2 border-b">{item.remainingTime} days</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 };
--- src/fetchYields.js
+++ src/fetchYields.js
@@ -1,38 +1,80 @@
 import gearboxLendingPoolABI from './abis/PoolV3.json';
+import farmingPoolABI from './abis/farmingPoolABI.json';
+import priceFeedABI from './abis/priceFeedABI.json';
+
 const {ethers, JsonRpcProvider} = require("ethers");
 
 const infuraProjectId = process.env.REACT_APP_INFURA_PROJECT_ID;
 const provider = new JsonRpcProvider(`https://arbitrum-mainnet.infura.io/v3/${infuraProjectId}`);
 
 const contracts = [
-  { protocol: 'Gearbox', chain: 'Arbitrum', asset: 'USDC', address: '0x890A69EF363C9c7BdD5E36eb95Ceb569F63ACbF6' },
-  { protocol: 'Gearbox', chain: 'Arbitrum', asset: 'USDC.e', address: '0xa76c604145D7394DEc36C49Af494C144Ff327861' },
+  { protocol: 'Gearbox', chain: 'Arbitrum', asset: 'USDC', address: '0x890A69EF363C9c7BdD5E36eb95Ceb569F63ACbF6', farmingPoolAddress: '0xD0181a36B0566a8645B7eECFf2148adE7Ecf2BE9' },
+  { protocol: 'Gearbox', chain: 'Arbitrum', asset: 'USDC.e', address: '0xa76c604145D7394DEc36C49Af494C144Ff327861', farmingPoolAddress: '0x'},
   // Add more contracts as needed
 ];
+const priceFeedAddress = '0xb2A824043730FE05F3DA2efaFa1CBbe83fa548D6';
 
 const convertSupplyRateToPercentage = (supplyRate) => {
-  // Convert BigNumber to a regular number
   const rate = ethers.formatUnits(supplyRate, 27); // Format to a number with 27 decimal places
   
-  // Convert to percentage
-  const percentage = parseFloat(rate) * 100; // Multiply by 100 to get percentage
+  const percentage = parseFloat(rate) * 100;
 
-  return percentage.toFixed(2); // Fix to 2 decimal places for display
+  return percentage.toFixed(2);
+};
+
+const calculateRewardsAPY = (priceData, totalSupply, duration, reward, finished) => {
+  const balance = parseFloat(ethers.formatUnits(totalSupply, 6))
+  const arbPrice = ethers.formatUnits(priceData.answer, 8); // Price with 8 decimals
+  const currentTime = Math.floor(Date.now() / 1000);
+  const elapsedTime = Math.min(currentTime, finished) - (finished - duration);
+  const remainingTime = finished - currentTime;
+  const distributedRewards = (elapsedTime * reward) / duration;
+  const remainingRewards = reward - distributedRewards;
+  const rewardsPerSecond = remainingRewards / remainingTime;
+  const rewardsPerSecondInUSD = rewardsPerSecond * arbPrice;
+  const rewardsPerYear = rewardsPerSecondInUSD * (60 * 60 * 24 * 365.25);
+  const rewardsAPY = (rewardsPerYear / balance * 100).toFixed(2);
+  return {rewardsAPY, remainingTime};
 };
 
 export async function fetchYields() {
   const results = [];
-  
+  const priceFeedContract = new ethers.Contract(priceFeedAddress, priceFeedABI, provider);
+
   for (const contractInfo of contracts) {
     const contract = new ethers.Contract(contractInfo.address, gearboxLendingPoolABI, provider);
+    var farmingPoolContract;
+    if (contractInfo.farmingPoolAddress !== '0x') {
+      farmingPoolContract = new ethers.Contract(contractInfo.farmingPoolAddress, farmingPoolABI, provider);
+      }
     try {
-      const supplyRate = await contract.supplyRate(); // Call the contract's supplyRate function
+      const supplyRate = await contract.supplyRate();
       const apyPercentage = convertSupplyRateToPercentage(supplyRate);
+      
+      const priceData = await priceFeedContract.latestRoundData(); // Fetch the latest ARB/USD price
+      const totalSupply = await contract.totalSupply();
+      const farmInfo = await farmingPoolContract.farmInfo();
+      
+      const duration = parseFloat(farmInfo.duration);
+      const reward = parseFloat(ethers.formatUnits(farmInfo.reward, 18)); // Assuming reward is in 18 decimals
+      const finished = parseFloat(farmInfo.finished);
+      var rewardsAPY, remainingTime;
+      if (contractInfo.farmingPoolAddress !== '0x') {
+        const result = calculateRewardsAPY(priceData, totalSupply, duration, reward, finished);
+        ({ rewardsAPY, remainingTime } = result);
+      } else {
+        rewardsAPY = 0;
+        remainingTime = 0;
+      }
       results.push({
         protocol: contractInfo.protocol,
         chain: contractInfo.chain,
         asset: contractInfo.asset,
+        totalSupply: parseFloat(ethers.formatUnits(totalSupply, 6)).toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
         apy: apyPercentage,
+        rewards: rewardsAPY,
+        remainingTime: (remainingTime / (60 * 60 * 24)).toFixed(2),
+        totalAPY: (parseFloat(apyPercentage) + parseFloat(rewardsAPY)).toFixed(2),
       });
     } catch (error) {
       console.error(`Error fetching APY for ${contractInfo.asset}:`, error);
@@ -45,5 +87,7 @@ export async function fetchYields() {
     }
   }
   
+
+
   return results;
 }
